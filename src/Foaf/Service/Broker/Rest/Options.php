6161252733a483c5fabbb3a1e0f9cb634b4d8201
Juha Suni <juha.suni@mediacabinet.fi>
2012-08-09 13:43:19 +0300

    Fixed base class name

--- src/Foaf/Service/Broker/Rest/Options.php
+++ src/Foaf/Service/Broker/Rest/Options.php
@@ -1,85 +1,90 @@
 <?php
 namespace Foaf\Service\Broker\Rest;
 
-use Zend\Stdlib\Options,
-	Zend\Mvc\Router;
+use Zend\Stdlib\AbstractOptions;
+use Zend\Mvc\Router;
 
-class Options extends Options{
-    
+class Options extends AbstractOptions
+{
+
     /**
      * Default server name
-     * 
+     *
      * @var string
      */
     protected $defaultServer = '';
-    
+
     /**
      * Array of servers
-     * 
+     *
      * @var array
      */
     protected $servers = array();
-    
+
     /**
      * Set default server name
-     * 
-     * @param string $server
+     *
+     * @param string $server            
      */
-    public function setDefaultServer($server){
+    public function setDefaultServer($server)
+    {
         $this->defaultServer = $server;
     }
-    
+
     /**
      * Retrieve default server name
-     * 
+     *
      * @return string
      */
-    public function getDefaultServer(){
+    public function getDefaultServer()
+    {
         return $this->defaultServer;
     }
-    
+
     /**
      * Set server configurations
-     * 
-     * @param array|\Traversable $servers
+     *
+     * @param array|\Traversable $servers            
      * @throws \InvalidArgumentException
      */
-    public function setServers($servers){
-        if(!is_array($servers) && !($servers instanceof \Traversable)){
-            throw new \InvalidArgumentException('$$servers must be an array or implement Traversable interface');
+    public function setServers($servers)
+    {
+        if (! is_array($servers) && ! ($servers instanceof \Traversable)) {
+            throw new \InvalidArgumentException(
+                    '$servers must be an array or implement Traversable interface');
         }
         
         $this->servers = array();
         
-        foreach ($servers as $name => $specs){
+        foreach ($servers as $name => $specs) {
             
-            $url 	= $specs['route'];
-            $route 	= new Router\Http\Segment($url);
-
+            $url = $specs['route'];
+            $route = new Router\Http\Segment($url);
+            
             $this->servers[$name] = array(
-            	'route' => $route
+                    'route' => $route
             );
         }
     }
-    
+
     /**
      * Retrieve server configurations
-     * 
+     *
      * @return array
      */
-    public function getServers(){
+    public function getServers()
+    {
         return $this->servers;
     }
-    
+
     /**
      * Retrieve server specs by name
-     * 
-     * @param string $name
-     * @return null|Array
+     *
+     * @param string $name            
+     * @return null Array
      */
-    public function getServerByName($name){
-        return 	isset( $this->servers[$name])
-        		? $this->servers[$name]
-        		: null;
+    public function getServerByName($name)
+    {
+        return isset($this->servers[$name]) ? $this->servers[$name] : null;
     }
 }
