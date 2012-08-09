3fde3b94535feef78038dab91a7643b8a9e17603
Juha Suni <juha.suni@mediacabinet.fi>
2012-08-09 13:55:15 +0300

    Support for new features, including plugins (service helpers)

--- src/Foaf/Service/AbstractService.php
+++ src/Foaf/Service/AbstractService.php
@@ -6,16 +6,25 @@ use Foaf\Service\ServiceInterface;
 use Foaf\Service\ServiceEvent;
 use Foaf\Service\Exception;
 use Foaf\Service\Feature;
+use Foaf\Service\Broker;
 use Foaf\Service\Invoker\InvokerInterface;
+use Foaf\Service\Plugin\PluginManager;
 use Foaf\Service\Definition;
 use Foaf\Service\Definition\Driver\PhpDriver;
 use Foaf\Service\Definition\DriverInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
 
 /**
  * @foaf\service\ignore
  */
 abstract class AbstractService 
-    implements ServiceInterface, Feature\InvokerAwareInterface, Feature\ConfigurableInterface, Feature\DefinitionProviderInterface
+    implements  ServiceInterface, 
+                Feature\InvokerAwareInterface, 
+                Feature\ConfigurableInterface, 
+                Feature\DefinitionProviderInterface,
+                Feature\ServiceBrokerAwareInterface,
+                ServiceLocatorAwareInterface
 {
     
     /**
@@ -54,6 +63,34 @@ abstract class AbstractService
 	private $invoker;
 	
 	/**
+	 * Service event instance
+	 * 
+	 * @var Foaf\Service\ServiceEvent
+	 */
+	private $event;
+	
+	/**
+	 * Service locator instance
+	 * 
+	 * @var Zend\ServiceManager\ServiceLocatorInterface
+	 */
+	private $serviceLocator;
+	
+	/**
+	 * Service broker instance
+	 * 
+	 * @var Broker
+	 */
+	private $serviceBroker;
+	
+	/**
+	 * Plugin manager instance
+	 * 
+	 * @var Foaf\Service\Plugin\PluginManager
+	 */
+	private $pluginManager;
+	
+	/**
 	 * Static definition driver
 	 * 
 	 * @var DriverInterface
@@ -62,7 +99,17 @@ abstract class AbstractService
 	
 	public function __invoke(ServiceEvent $e)
 	{
+	    $this->event = $e;
 	    return $this->getInvoker()->invoke($this, $e);
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see \Foaf\Service\ServiceInterface::getEvent()
+	 */
+	public function getEvent()
+	{
+	    return $this->event;
 	}
 	
 	/**
@@ -150,6 +197,73 @@ abstract class AbstractService
 		$this->setOptions($config);
 	}
 	
+	/**
+	 * Retrieve service locator instance
+	 *
+	 * @return \Zend\ServiceLocator\ServiceLocatorInterface
+	 */
+	public function getServiceLocator()
+	{
+	    return $this->serviceLocator;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see Zend\ServiceLocator\ServiceLocator::setServiceLocator()
+	 */
+	public function setServiceLocator(ServiceLocatorInterface $serviceLocator){
+	    $this->serviceLocator = $serviceLocator;
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see \Foaf\Service\Feature\ServiceBrokerAwareInterface::setServiceBroker()
+	 */
+	public function setServiceBroker(Broker $broker)
+	{
+	    $this->serviceBroker = $broker;
+	}
+	
+	/**
+	 * Retrieve service broker instance
+	 * 
+	 * @return \Foaf\Service\Broker
+	 */
+	public function getServiceBroker()
+	{
+	    return $this->serviceBroker;
+	}
+	
+	/**
+	 * Retrieve plugin manager instance
+	 * 
+	 * @return \Foaf\Service\Plugin\PluginManager
+	 */
+	public function getPluginManager()
+	{
+	    if(!$this->pluginManager){
+	        $this->setPluginManager(new PluginManager());
+	    }
+	    
+	    return $this->pluginManager;
+	}
+	
+	/**
+	 * Set plugin manager instance
+	 * 
+	 * @param PluginManager
+	 * @return Foaf\Service\AbstractService
+	 */
+	public function setPluginManager(PluginManager $pluginManager)
+	{
+	    $pluginManager->setFoafService($this);
+	    $pluginManager->setServiceBroker($this->getServiceBroker());
+	    
+	    $this->pluginManager = $pluginManager;
+	    
+	    return $this;
+	}
+	
     /**
      * Set service options
      *
@@ -158,7 +272,17 @@ abstract class AbstractService
      */
 	public function setOptions($options)
     { 
-    	$this->options = new $this->optionsClass($options);
+        if (!is_array($options) && !$options instanceof \Traversable) {
+            throw new \InvalidArgumentException(sprintf(
+                'Parameter provided to %s must be an array or Traversable',
+                __METHOD__
+            ));
+        }
+        
+        foreach ($options as $key => $value){
+            $this->setOption($key, $value);
+        }
+    	
         return $this;
     }
     
@@ -209,6 +333,39 @@ abstract class AbstractService
     public function getOption($key)
     {
         return $this->getOptions()->__get($key);
+    }
+
+    /**
+     * Get plugin instance
+     *
+     * @param  string     $name    Name of plugin to return
+     * @param  null|array $options Options to pass to plugin constructor (if not already instantiated)
+     * @return mixed
+     */
+    public function plugin($name, array $options = null)
+    {
+        return $this->getPluginManager()->get($name, $options);
+    }
+    
+    /**
+     * Method overloading: return/call plugins
+     *
+     * If the plugin is a functor, call it, passing the parameters provided.
+     * Otherwise, return the plugin instance.
+     *
+     * @param  string $method
+     * @param  array $params
+     * @return mixed
+     */
+    public function __call($method, array $params)
+    {
+        $plugin = $this->plugin($method);
+        
+        if (is_callable($plugin)) {
+            return call_user_func_array($plugin, $params);
+        }
+        
+        return $plugin;
     }
     
     /**
