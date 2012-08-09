4e2282de09634ac04d5297eb66b8619b22b83a1f
Juha Suni <juha.suni@mediacabinet.fi>
2012-08-09 13:45:11 +0300

    Service interface now provides access (getter) to service event

--- src/Foaf/Service/ServiceInterface.php
+++ src/Foaf/Service/ServiceInterface.php
@@ -3,5 +3,17 @@ namespace Foaf\Service;
 
 interface ServiceInterface
 {
+    /**
+     * Invoke operation based on event
+     * 
+     * @param ServiceEvent $e
+     */
     public function __invoke(ServiceEvent $e);
+    
+    /**
+     * Retrieve current event instance
+     * 
+     * @return ServiceEvent
+     */
+    public function getEvent();
 }
