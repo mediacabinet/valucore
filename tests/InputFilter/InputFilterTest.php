f80853c72a7138c3dac9aecf54dd7571b5f58a4e
Juha Suni <juha.suni@mediacabinet.fi>
2013-02-06 15:02:02 +0200

    Added tests for service locator injection

---
+++ tests/InputFilter/InputFilterTest.php
@@ -0,0 +1,66 @@
+<?php
+namespace Valu\InputFilter;
+
+use Zend\InputFilter\Input;
+
+use Zend\ServiceManager\ServiceManager;
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class InputFilterTest extends TestCase
+{
+    public function testSetGetServiceLocator()
+    {
+        $serviceLocator = new ServiceManager();
+        $inputFilter = new InputFilter();
+        $inputFilter->setServiceLocator($serviceLocator);
+        
+        $this->assertEquals($serviceLocator, $inputFilter->getServiceLocator());
+    }
+    
+    public function testFilterChainInheritsServiceLocator()
+    {
+        $serviceLocator = new ServiceManager();
+        
+        $input = new Input('test');
+        $input->getFilterChain()->getPluginManager()->setInvokableClass(
+            'testfilter', 
+            'Valu\Test\InputFilter\TestAsset\TestFilter');
+        
+        $input->getFilterChain()->attachByName('testfilter');
+        
+        $inputFilter = new InputFilter();
+        $inputFilter->add($input);
+        
+        // Inject service locator
+        $inputFilter->setServiceLocator($serviceLocator);
+        
+        $this->assertEquals(
+            $serviceLocator,
+            $input->getFilterChain()->getFilters()->top()->getServiceLocator()->getServiceLocator());
+    }
+    
+    public function testValidatorChainInheritsServiceLocator()
+    {
+        $serviceLocator = new ServiceManager();
+        
+        $input = new Input('test');
+        $input->getValidatorChain()->getPluginManager()->setInvokableClass(
+                'testvalidator',
+                'Valu\Test\InputFilter\TestAsset\TestValidator');
+        
+        $input->getValidatorChain()->addByName('testvalidator');
+        
+        $inputFilter = new InputFilter();
+        $inputFilter->add($input);
+        
+        // Inject service locator
+        $inputFilter->setServiceLocator($serviceLocator);
+        
+        $validators = $input->getValidatorChain()->getValidators();
+        
+        $this->assertEquals(
+            $serviceLocator,
+            $validators[0]['instance']->getServiceLocator()->getServiceLocator());
+    }
+}
