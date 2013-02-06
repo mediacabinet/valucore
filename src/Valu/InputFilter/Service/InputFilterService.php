33dfa033d2a14460fb6559d8431759b9122e7c1f
Juha Suni <juha.suni@mediacabinet.fi>
2013-02-06 15:00:57 +0200

    Service is now service locator aware, so that it can inject it to inputfilter

--- src/Valu/InputFilter/Service/InputFilterService.php
+++ src/Valu/InputFilter/Service/InputFilterService.php
@@ -1,13 +1,16 @@
 <?php
 namespace Valu\InputFilter\Service;
 
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 use Valu\InputFilter\InputFilterRepository;
 use Valu\Service\Exception\OperationNotFoundException;
 use Valu\Service\ServiceInterface;
 use Valu\Service\ServiceEvent;
 
 class InputFilterService
-    implements ServiceInterface
+    implements  ServiceInterface, 
+                ServiceLocatorAwareInterface
 {
     /**
      * Input filter repository
@@ -22,6 +25,13 @@ class InputFilterService
      * @var ServiceEvent
      */
     protected $event;
+    
+    /**
+     * Service locator
+     *
+     * @var ServiceLocatorInterface
+     */
+    private $serviceLocator;
     
     public static function version()
     {
@@ -52,18 +62,59 @@ class InputFilterService
         }
     }
     
+    /**
+     * @see \Valu\Service\ServiceInterface::getEvent()
+     */
     public function getEvent()
     {
         return $this->event;
     }
     
+    /**
+     * Retrieve input filter by name
+     * 
+     * @param string $name
+     * @return \Zend\InputFilter\InputFilterInterface|null
+     */
     public function get($name)
     {
-        return $this->repository->get($name);
+        $inputFilter = $this->repository->get($name);
+        
+        if ($inputFilter && 
+            $inputFilter instanceof ServiceLocatorAwareInterface && 
+            !$inputFilter->getServiceLocator() &&
+            $this->getServiceLocator()) {
+            
+            $inputFilter->setServiceLocator($this->getServiceLocator());
+        }
+        
+        return $inputFilter;
     }
     
+    /**
+     * Reload input filter
+     * 
+     * @param string $name
+     * @return boolean
+     */
     public function reload($name)
     {
         return $this->repository->reload($name);
+    }
+    
+    /**
+     * @see \Zend\ServiceManager\ServiceLocatorAwareInterface::getServiceLocator()
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+    
+    /**
+     * @see \Zend\ServiceManager\ServiceLocatorAwareInterface::setServiceLocator()
+     */
+    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
+    {
+        $this->serviceLocator = $serviceLocator;
     }
 }
