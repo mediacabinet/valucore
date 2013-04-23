07385f75d14b01ceb4858da3f4771793a228a5ed
Juha Suni <juha.suni@mediacabinet.fi>
2013-04-23 18:18:48 +0300

    Adds some missing basic configs

--- config/module.config.php
+++ config/module.config.php
@@ -2,7 +2,15 @@
 return [
     'service_manager' => [
         'factories' => [
+            'ValuModelArrayAdapter' => 'Valu\\Model\\ServiceManager\\ArrayAdapterFactory',
             'valu.inputfilter.repository' => 'Valu\InputFilter\ServiceManager\InputFilterRepositoryFactory',
+        ],
+        'aliases' => [
+            'Cache' => 'ValuCache',
+            'ArrayAdapter' => 'ValuModelArrayAdapter',
+        ],
+        'shared' => [
+            'ValuModelArrayAdapter' => false
         ]
     ],
     'valu_so' => [
