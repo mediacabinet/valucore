dd815917f3fed9442f74a4109934bcde78f322c3
Juha Suni <juha.suni@mediacabinet.fi>
2014-05-22 13:43:56 +0300

    Removes autoloader bootstrapping

--- src/Valu/Module.php
+++ src/Valu/Module.php
@@ -2,30 +2,10 @@
 namespace ValuCore;
 
 use Zend\ModuleManager\Feature;
-use Zend\Loader\AutoloaderFactory;
-use Zend\Loader\StandardAutoloader;
 
 class Module
-    implements Feature\AutoloaderProviderInterface,
-               Feature\ConfigProviderInterface
+    implements Feature\ConfigProviderInterface
 {
-    /**
-     * getAutoloaderConfig() defined by AutoloaderProvider interface.
-     *
-     * @see AutoloaderProvider::getAutoloaderConfig()
-     * @return array
-     */
-    public function getAutoloaderConfig()
-    {
-        return array(
-            AutoloaderFactory::STANDARD_AUTOLOADER => array(
-                StandardAutoloader::LOAD_NS => array(
-                    'Valu' => __DIR__,
-                ),
-            ),
-        );
-    }
-    
     /**
      * getConfig implementation for ConfigListener
      *
