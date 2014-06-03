0caad6e93aabbe1d55a90c04678186bc9e1d64bb
Juha Suni <juha.suni@mediacabinet.fi>
2014-06-03 13:52:30 +0300

    Configures Module path

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,5 +1,4 @@
 <?php
 return array(
-    'Valu\Module' => __DIR__ . '/src/Valu/Module.php',
-    'ValuCore\Module' => __DIR__ . '/src/Valu/Module.php',
+    'ValuCore\Module' => __DIR__ . '/src/ValuCore/Module.php',
 );
